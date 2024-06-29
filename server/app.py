3607af49f795ba4087cc937811723287f0282acb
Nicole Apono <napononicole@gmail.com>
2024-06-30 02:09:49 +0300

    add a view that takes one parameter, an integer that represents an id

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -21,7 +21,40 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    if earthquake:
+        response = {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        return make_response(jsonify(response), 200)
+    else:
+        error_response = {
+            'message': f'Earthquake {id} not found.'
+        }
+        return make_response(jsonify(error_response), 404)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    earthquake_list = [
+        {
+            'id': earthquake.id,
+            'location': earthquake.location,
+            'magnitude': earthquake.magnitude,
+            'year': earthquake.year
+        }
+        for earthquake in earthquakes
+    ]
+    response = {
+        'count': len(earthquake_list),
+        'quakes': earthquake_list
+    }
+    return make_response(jsonify(response), 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
